c28463d92b143c7969fb93fd3a7f80730bb0a6a4
wlsdbdh <129045769+wlsdbdh@users.noreply.github.com>
2023-07-05 19:03:10 +0900

    Add files via upload

---
+++ deeplearning-from-scratch/ch3/mnist_show.py
@@ -0,0 +1,26 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from dataset.mnist import load_mnist
+from PIL import Image
+
+def img_show(img) :
+    pil_image = Image.fromarray(np.uint8(img)) # PIL Image를 Numpy array로 변환
+    pil_image.show()
+
+
+(x_train, t_train), (x_test, t_test) = load_mnist (flatten=True, normalize=False)
+
+print (x_train.shape)
+print (t_train.shape)
+print (x_test.shape)
+print (t_test.shape)
+
+img = x_train[0]
+label = t_train[0]
+print (label)
+
+print (img.shape)
+img = img.reshape(28, 28)
+print (img.shape)
+img_show(img)
